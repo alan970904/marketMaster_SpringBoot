75dbf1fb8601d4185ee6e1ebcf3c4f91138e9c1d
new <sam05091998@gmail.com>
2024-10-07 16:38:14 +0800

    新增供應商功能

--- src/main/java/marketMaster/bean/restock/SupplierAccountsBean.java
+++ src/main/java/marketMaster/bean/restock/SupplierAccountsBean.java
@@ -1,53 +1,57 @@
-    package marketMaster.bean.restock;
+package marketMaster.bean.restock;
 
-    import com.fasterxml.jackson.annotation.JsonIgnore;
-    import jakarta.persistence.*;
-    import lombok.AllArgsConstructor;
-    import lombok.Getter;
-    import lombok.NoArgsConstructor;
-    import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import marketMaster.bean.restock.PaymentsBean;
+import marketMaster.bean.restock.SuppliersBean;
 
-    import java.util.Set;
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Entity
-    @Table(name = "supplier_accounts")
-    public class SupplierAccountsBean {
+import java.util.Set;
 
-        @Id
-        @Column(name = "account_id")
-        private String accountId;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "supplier_accounts")
+public class SupplierAccountsBean {
 
-        @OneToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "supplier_id")
-        @JsonIgnore
-        private SuppliersBean supplier;
+    @Id
+    @Column(name = "account_id")
+    private String accountId;
 
-        @OneToMany(mappedBy = "supplierAccounts", cascade = CascadeType.ALL)
-        @JsonIgnore
-        private Set<PaymentsBean> payments;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_id")
+    @JsonIgnore
+    private SuppliersBean supplier;
 
+    @OneToMany(mappedBy = "supplierAccounts", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<PaymentsBean> payments;
 
-        @Column(name = "total_amount")
-        private int totalAmount;
+    @Column(name = "total_amount")
+    private int totalAmount;
 
-        @Column(name = "paid_amount")
-        private int paidAmount;
+    @Column(name = "paid_amount")
+    private int paidAmount;
 
-        @Column(name = "unpaid_amount")
-        private int unpaidAmount;
+    @Column(name = "unpaid_amount")
+    private int unpaidAmount;
 
-        @Override
-        public String toString() {
-            return "SupplierAccountsBean{" +
-                    "accountId='" + accountId + '\'' +
-                    // 避免遞歸調用
-                    // ", supplier=" + supplier +
-                    ", totalAmount=" + totalAmount +
-                    ", paidAmount=" + paidAmount +
-                    ", unpaidAmount=" + unpaidAmount +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "SupplierAccountsBean{" +
+                "accountId='" + accountId + '\'' +
+                // 避免递归调用
+                // ", supplier=" + supplier +
+                ", totalAmount=" + totalAmount +
+                ", paidAmount=" + paidAmount +
+                ", unpaidAmount=" + unpaidAmount +
+                '}';
     }
+
+
+}
--- src/main/java/marketMaster/service/restock/SupplierService.java
+++ src/main/java/marketMaster/service/restock/SupplierService.java
@@ -2,17 +2,13 @@ package marketMaster.service.restock;
 
 import marketMaster.DTO.restock.SupplierDTO.SupplierIdAndNameDTO;
 import marketMaster.DTO.restock.SupplierDTO.SupplierInfoDTO;
-import marketMaster.bean.restock.SupplierAccountsBean;
 import marketMaster.bean.restock.SuppliersBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Supplier;
 
 @Service
 public class SupplierService {
@@ -29,8 +25,8 @@ public class SupplierService {
         return suppliersRepository.findById(supplierId).orElse(null);
     }
     //新增 新的供應商
-    public SuppliersBean saveSupplier(SuppliersBean suppliersBean) {
-        return suppliersRepository.save(suppliersBean);
+    public void saveSupplier(SuppliersBean suppliersBean) {
+        suppliersRepository.save(suppliersBean);
     }
     //透過id更新供應商
     @Transactional
@@ -66,12 +62,23 @@ public class SupplierService {
     public List<SupplierIdAndNameDTO>findAllSupplierIdAndName(){
         return suppliersRepository.findAllSupplierIdAndName();
     }
+    //拿到最新的供應商Id
+    public String getLastSupplierId() {
+          String lastSupplierId= suppliersRepository.getLastSupplierId();
+          if(lastSupplierId==null){
+              return "S001";
+          }
+          int Id=Integer.parseInt(lastSupplierId.substring(1));
+          int nextID = Id+1;
+        return String.format("S%03d", nextID);
+    }
+
 
 
     //更新供應商詳細資料但是稅務號碼沒改
     @Transactional
     @Modifying
-    public void updateSupplierBySupplierId(SupplierInfoDTO supplierInfoDTO) {
+    public String updateSupplierBySupplierId(SupplierInfoDTO supplierInfoDTO) {
          String supplierID = supplierInfoDTO.getSupplierId();
          SuppliersBean supplierBean = findSupplierById(supplierID);
          supplierBean.setSupplierName(supplierInfoDTO.getSupplierName());
@@ -80,6 +87,7 @@ public class SupplierService {
          supplierBean.setPhone(supplierInfoDTO.getPhone());
          supplierBean.setEmail(supplierInfoDTO.getEmail());
          suppliersRepository.save(supplierBean);
+        return supplierID;
     }
 
 
