ecf9c3ef07e0c95ed97d0719e9c84d86097570c9
Yee <z5se6fv@gmail.com>
2024-10-07 22:32:20 +0800

    商品模糊搜尋完成

--- src/main/java/marketMaster/controller/product/ProductRestController.java
+++ src/main/java/marketMaster/controller/product/ProductRestController.java
@@ -1,17 +1,12 @@
 package marketMaster.controller.product;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import marketMaster.DTO.product.ProductNameDTO;
 import marketMaster.DTO.product.ProductPageDTO;
 import marketMaster.bean.product.ProductBean;
 import marketMaster.service.product.ProductService;
@@ -22,16 +17,6 @@ public class ProductRestController {
 	@Autowired
 	private ProductService productService;
 
-	@GetMapping("/product/findAll/json")
-	public ResponseEntity<ProductPageDTO> getAllProduct(
-			@RequestParam(value = "page", defaultValue = "1") Integer pageNumber,
-			@RequestParam(value = "size", defaultValue = "10") Integer pageSize) {
-
-		Page<ProductBean> products = productService.findAllProduct(pageNumber, pageSize);
-		ProductPageDTO dto = new ProductPageDTO(products);
-		return ResponseEntity.ok(dto);
-	}
-
 	@GetMapping("/product/findOne/json")
 	public ProductBean getOneProduct(@RequestBody ProductBean productBean) {
 		ProductBean product = productService.findOneProduct(productBean.getProductId());
@@ -40,9 +25,9 @@ public class ProductRestController {
 	}
 	
 	@PostMapping("/product/findProductByLike/json")
-	public Page<ProductBean> getProductsByLike(@RequestParam(value = "page", defaultValue = "1") Integer pageNumber,
-			 @RequestBody ProductNameDTO searchName){
-		String productName = searchName.getProductName();
+	public Page<ProductBean> getProductsByLike(@RequestBody ProductPageDTO productPageDTO){
+		String productName = productPageDTO.getProductName();
+		Integer pageNumber = productPageDTO.getPageNumber();
 		Page<ProductBean> products = productService.findProductByLike(productName,pageNumber);
 		
 		return products;
