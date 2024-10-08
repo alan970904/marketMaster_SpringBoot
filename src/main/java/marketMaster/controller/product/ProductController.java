afe89a6ac6a517e33757c08b8ab1ff77870ad1ff
Yee <z5se6fv@gmail.com>
2024-10-08 23:45:41 +0800

    商品頁面套用樣板ok

--- src/main/java/marketMaster/controller/product/ProductController.java
+++ src/main/java/marketMaster/controller/product/ProductController.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import marketMaster.DTO.product.ProductCategoryDTO;
@@ -110,8 +108,7 @@ public class ProductController {
 	@Transactional
 	@PostMapping("/product/remove")
 	public String postMethodName(@RequestParam String productId) {
-		ProductBean product = productService.removeProduct(productId);
-		
+		productService.removeProduct(productId);
 
 		return "redirect:/product/findAll";
 	}
