04ef6a18c7c6d679dbba065fe29bd0d3c68bc465
FanJava187 <ricekevin22@gmail.com>
2024-10-18 03:03:42 +0800

    消息通知圖示尚未完成

--- src/main/java/marketMaster/service/notification/NotificationService.java
+++ src/main/java/marketMaster/service/notification/NotificationService.java
@@ -1,7 +1,16 @@
 package marketMaster.service.notification;
 
+import java.util.List;
+
+import marketMaster.bean.notification.Notification;
+
 public interface NotificationService {
 
 	void sendNotificationByEvent(String event, String[] roles);
+
+	List<Notification> getLatestNotifications(String userId, int limit);
+
+	void markAllAsRead(String userId);
+
 
 }
