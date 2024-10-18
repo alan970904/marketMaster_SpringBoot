1778a6a06b2ba3f74d1a9cf0ee2ec8f4be5df884
Yee <z5se6fv@gmail.com>
2024-10-18 16:34:08 +0800

    盤點頁面樣板

--- src/main/java/marketMaster/controller/product/InventoryCheckController.java
+++ src/main/java/marketMaster/controller/product/InventoryCheckController.java
@@ -1,19 +1,44 @@
 package marketMaster.controller.product;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import marketMaster.bean.product.InventoryCheckBean;
+import marketMaster.bean.product.ProductBean;
 import marketMaster.service.product.InventoryCheckService;
+import marketMaster.service.product.ProductService;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class InventoryCheckController {
 
 	@Autowired
 	private InventoryCheckService inventoryCheckService;
+	
+	@Autowired
+	private ProductService productService;
 
+	@GetMapping("/inventoryCheck/getAllProduct")
+	public String getAllProduct(@RequestParam(value = "page", defaultValue = "1") Integer pageNumber,
+			@RequestParam(value = "size", defaultValue = "10") Integer pageSize, Model m) {
+		Page<ProductBean> products = productService.findAllProduct(pageNumber, pageSize);
+		List<InventoryCheckBean> inventoryCheck = inventoryCheckService.findAllInventoryCheck();
+		m.addAttribute("products", products);
+		m.addAttribute("inventoryCheck", inventoryCheck);
+		return "/product/test";
+	}
+	
+	
 	@ResponseBody
 	@GetMapping("/inventoryCheck/findByCheckId")
 	public InventoryCheckBean findDetailsByCheckId(String inventoryCheckId) {
@@ -21,4 +46,18 @@ public class InventoryCheckController {
 		return details;
 
 	}
+	
+	@PostMapping("/inventoryCheck/addCheck")
+	public InventoryCheckBean addInventoryCheck(@RequestParam InventoryCheckBean inventoryCheckBean) {
+		inventoryCheckService.addInventoryCheck(inventoryCheckBean);
+		
+		return null;
+	}
+	
+	@PostMapping("/inventoryCheck/delete")
+	public String deleteInventoryCheck(@RequestParam String inventoryCheckId) {
+		inventoryCheckService.deleteInventoryCheck(inventoryCheckId);
+		return null;
+	}
+	
 }
--- src/main/java/marketMaster/service/product/InventoryCheckService.java
+++ src/main/java/marketMaster/service/product/InventoryCheckService.java
@@ -1,18 +1,24 @@
 package marketMaster.service.product;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import marketMaster.bean.product.InventoryCheckBean;
+import marketMaster.bean.product.InventoryCheckDetailsBean;
 
 @Service
 public class InventoryCheckService {
 
 	@Autowired
 	private InventoryCheckRepository inventoryCheckRepo;
-	
+
+	@Autowired
+	private InventoryCheckDetailsRepository inventoryCheckDetailsRepo;
+
 	public InventoryCheckBean findDetailsBycheckId(String inventoryCheckId) {
 		Optional<InventoryCheckBean> optional = inventoryCheckRepo.findById(inventoryCheckId);
 		if (optional.isPresent()) {
@@ -20,5 +26,55 @@ public class InventoryCheckService {
 			return inventoryCheck;
 		}
 		return null;
+	}
+	
+	public List<InventoryCheckBean> findAllInventoryCheck() {
+		List<InventoryCheckBean> inventoryChecks = inventoryCheckRepo.findAll();
+		return inventoryChecks;
+	}
+
+	public InventoryCheckBean addInventoryCheck(InventoryCheckBean inventoryCheckBean) {
+		String inventoryCheckId = inventoryCheckBean.getInventoryCheckId();
+		Optional<InventoryCheckBean> optional = inventoryCheckRepo.findById(inventoryCheckId);
+		LocalDate now = LocalDate.now();
+		if (optional.isEmpty()) {
+			InventoryCheckBean inventoryCheck = optional.get();
+			inventoryCheck.setInventoryCheckDate(now);
+			inventoryCheckRepo.save(inventoryCheck);
+			List<InventoryCheckDetailsBean> detailsList = inventoryCheck.getDetails();
+			for (InventoryCheckDetailsBean detail : detailsList) {
+				inventoryCheckDetailsRepo.save(detail);
+			}
+
+		}
+		return null;
+	}
+
+	public InventoryCheckBean updateInventoryCheck(InventoryCheckBean inventoryCheckBean) {
+		String inventoryCheckId = inventoryCheckBean.getInventoryCheckId();
+		Optional<InventoryCheckBean> optional = inventoryCheckRepo.findById(inventoryCheckId);
+
+		if (optional.isPresent()) {
+			InventoryCheckBean oldInventoryCheck = optional.get();
+			return null;
+		}
+		return null;
+	}
+
+	public void deleteInventoryCheck(String inventoryCheckId) {
+		inventoryCheckRepo.deleteById(inventoryCheckId);
+	}
+
+	public String newId() {
+
+		String maxId = inventoryCheckDetailsRepo.findMaxId();
+		if (maxId == null || maxId.isEmpty()) {
+			return "IC001";
+		}
+		String eng = maxId.substring(0, 3);
+		String numStr = maxId.substring(3);
+		int num = Integer.parseInt(numStr);
+		String result = eng + String.format("%03d", num + 1);
+		return result;
 	}
 }
