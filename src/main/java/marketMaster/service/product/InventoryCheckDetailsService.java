ed36806648753f1fd270d3c61bcb2d87d5aaf16b
Yee <z5se6fv@gmail.com>
2024-10-18 16:34:08 +0800

    盤點頁面樣板

--- src/main/java/marketMaster/service/product/InventoryCheckDetailsService.java
+++ src/main/java/marketMaster/service/product/InventoryCheckDetailsService.java
@@ -13,20 +13,41 @@ public class InventoryCheckDetailsService {
 
 	@Autowired
 	private InventoryCheckDetailsRepository inventoryCheckDetailsRepo;
-	
+
 	public InventoryCheckDetailsBean findOneInventoryCheckDetailById(String detailId) {
 		Optional<InventoryCheckDetailsBean> optional = inventoryCheckDetailsRepo.findById(detailId);
-		
+
 		if (optional.isPresent()) {
 			InventoryCheckDetailsBean inventoryCheckDetail = optional.get();
 			return inventoryCheckDetail;
 		}
 		return null;
 	}
-	
+
 	public List<InventoryCheckDetailsBean> findByInventoryCheckDetailId(String inventoryCheckId) {
-	
-		
+
 		return inventoryCheckDetailsRepo.findDetailByInventoryCheckId(inventoryCheckId);
+	}
+	
+	public InventoryCheckDetailsBean addInventoryCheckDetail(InventoryCheckDetailsBean inventoryCheckDetailsBean) {
+		String detailId = inventoryCheckDetailsBean.getDetailId();
+		Optional<InventoryCheckDetailsBean> optional = inventoryCheckDetailsRepo.findById(detailId);
+		if (optional.isEmpty()) {
+			return inventoryCheckDetailsRepo.save(inventoryCheckDetailsBean);
+		}
+		return null;
+	}
+	
+	public String newId() {
+		
+		String maxId = inventoryCheckDetailsRepo.findMaxId();
+		if (maxId == null || maxId.isEmpty()) {
+			return "ICD00000001";
+		}
+		String eng = maxId.substring(0, 3);
+		String numStr = maxId.substring(3);
+		int num = Integer.parseInt(numStr);
+		String result = eng+String.format("%08d", num+1);
+		return result;
 	}
 }
