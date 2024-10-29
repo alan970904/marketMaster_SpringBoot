2aa2cfad44efc8332e536ae814ed3418a1d88e9e
FanJava187 <ricekevin22@gmail.com>
2024-10-30 01:26:52 +0800

    加密加上註解

--- src/main/java/marketMaster/config/PasswordConfig.java
+++ src/main/java/marketMaster/config/PasswordConfig.java
@@ -5,9 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/*
+ * 加密配置類，
+ * 使用 Spring Security 提供的 BCryptPasswordEncoder 來進行密碼加密
+ */
 @Configuration
 public class PasswordConfig {
 
+	/*
+	 *  讓 Spring 容器管理 PasswordEncoder 實例，
+	 *  其他元件可以透過依賴注入來使用這個加密器
+	 */
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
