af7ba15ff3b94077566c30b53394ef985ad571ec
new <sam05091998@gmail.com>
2024-11-02 09:09:30 +0800

    更新商品選擇

--- src/main/java/marketMaster/controller/restock/SupplierProductsController.java
+++ src/main/java/marketMaster/controller/restock/SupplierProductsController.java
@@ -1,10 +1,14 @@
 package marketMaster.controller.restock;
 
+import marketMaster.DTO.restock.SupplierDTO.SupplierProductDTO;
 import marketMaster.bean.restock.SupplierProductsBean;
 import marketMaster.service.restock.SupplierProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @Controller
 @RequestMapping("/supplierProducts")
 public class SupplierProductsController {
@@ -29,6 +33,12 @@ public class SupplierProductsController {
     public void updateSupplierProduct(@RequestBody SupplierProductsBean supplierProductsBean) {
         System.out.println(supplierProductsBean);
         supplierProductsService.updateSupplierProduct(supplierProductsBean);
+    }
+
+    @GetMapping("/findAllProduct")
+    @ResponseBody
+    public List<SupplierProductDTO> findAllProduct() {
+        return supplierProductsService.findAllProductIdAndProductName();
     }
 
 
